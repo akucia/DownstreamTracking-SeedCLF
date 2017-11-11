a3daf2a2958551ef6ac172a81795b785f16a346c
akucia <art.kucia@gmail.com>
2017-11-11 13:38:39 +0100

    Rework project structure

--- Seed Classifier implementation/src/LWTNNClassifier.cpp
+++ Seed Classifier implementation/src/LWTNNClassifier.cpp
@@ -4,24 +4,34 @@
 
 #include "LWTNNClassifier.h"
 
-
+#include "lwtnn/parse_json.hh"
+#include "lwtnn/LightweightGraph.hh"
 
 LWTNNClassifier::LWTNNClassifier(int n) {
     using namespace lwt;
     // get your saved JSON file as an std::istream object
-
-    std::ifstream input(" ");
+    using namespace std;
+    std::ifstream input("neural_net.json");
+    
+//    cout << input. << endl;
     // build the graph
     LightweightGraph graph(parse_json_graph(input));
 
     // fill a map of input nodes
     float value = 1;
-    float value_2 = 2;
-    float another_value = 3;
-
+    
     std::map<std::string, std::map<std::string, double> > inputs;
-    inputs["input_node"] = {{"value", value}, {"value_2", value_2}};
-    inputs["another_input_node"] = {{"another_value", another_value}};
+    inputs["node_0"] = {
+            {"variable_0", value},
+            {"variable_1", value},
+            {"variable_2", value},
+            {"variable_3", value},
+            {"variable_4", value},
+            {"variable_5", value},
+            {"variable_6", value},
+            {"variable_7", value},
+            
+    };
     // compute the output values
     std::map<std::string, double> outputs = graph.compute(inputs);
     std::cout << "LWTNN DONE" << std::endl;
